81056b3b3a87bf11c2fa757e2a747661788abfd8
Myo-Khant11Aung <myokhantaung2004@email.com>
2025-12-30 17:05:39 -0800

    src addition

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-red-500 text-4xl font-bold p-6">
+      Tailwind v3 is working
+    </div>
+  );
+}
+
+export default App;
